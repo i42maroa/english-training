00a815898654c7dbd276d4f162321f534ca33796
Antonio Marín Rodríguez <amrodriguezjarote@gmail.com>
2022-07-29 14:48:12 +0200

    Add export pdf

--- src/app/core/services/word.service.ts
+++ src/app/core/services/word.service.ts
@@ -3,6 +3,10 @@ import { from, Observable } from 'rxjs';
 import { Word } from 'src/app/shared/models/word.interface';
 import { FirestoreService } from './firestore/firestore.service';
 
+import * as pdfMake from 'pdfmake/build/pdfmake.js';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts.js';
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
+
 @Injectable({
   providedIn: 'root'
 })
@@ -30,5 +34,38 @@ export class WordService {
 
   deleteWord(id:string){
     return from(this.firestore.deleteWord(id));
+  }
+
+  exportPdf(wordList:Word[]){
+
+    //[izq,arriba, der, abajo]
+    const tableBody = [[
+      {text:'WORD', fontSize: 16, bold: true, color:'#661F33',  margin: [0, 5, 20, 5]}, 
+      {text:'TRANSLATION', fontSize: 16, bold: true, color:'#661F33', margin: [0, 5, 20, 5]}
+    ]]
+
+    wordList.forEach(word =>{
+      tableBody.push([
+        {text:word.name, fontSize: 12, bold:false, color:'#30362F', margin: [0, 5, 20, 5]}, 
+        {text:word.translate, fontSize: 12, bold:false, color:'#30362F',  margin: [0, 5, 20, 5]}
+      ])
+    })
+    
+    const pdfCreate:any = {
+      content:[
+        {text: 'ENGLISH TRAINING', fontSize: 32, bold: true, color:'#661F33', margin: [0, 0, 0, 20]},
+        {text: 'Words List', fontSize: 18, color:'#30362F', bold: true, margin: [0, 0, 0, 20]},
+        {
+          style: 'tableExample',
+          table: {
+            body: tableBody
+          },
+          layout: 'noBorders'
+        },
+      ]
+    }
+
+    const pdf = pdfMake.createPdf(pdfCreate);
+    pdf.open()
   }
 }
--- src/app/page/landing-page/landing-page.component.ts
+++ src/app/page/landing-page/landing-page.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { selectShowModalWord } from 'src/app/state/selectors/words.selectors';
+import { WordService } from 'src/app/core/services/word.service';
+import { Word } from 'src/app/shared/models/word.interface';
+import { selectShowModalWord, selectWords } from 'src/app/state/selectors/words.selectors';
 
 @Component({
   selector: 'app-landing-page',
@@ -11,12 +13,19 @@ import { selectShowModalWord } from 'src/app/state/selectors/words.selectors';
 export class LandingPageComponent implements OnInit {
 
   showModal$:Observable<boolean> = new Observable<boolean>();
+  wordList$:Observable<ReadonlyArray<Word>> = new Observable<ReadonlyArray<Word>>();
 
   constructor(
-    private readonly store:Store
+    private readonly store:Store,
+    private readonly word:WordService
   ) { }
 
   ngOnInit(): void {
     this.showModal$ = this.store.select(selectShowModalWord);
+    this.wordList$= this.store.select(selectWords);
+  }
+
+  export(){
+    this.wordList$.subscribe(words => this.word.exportPdf(words as Word[]))
   }
 }
