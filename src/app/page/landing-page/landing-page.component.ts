f04bd9bfdfeeabd82b5cf677f712e797fb5b2555
Antonio Marín <amrodriguezjarote@gmail.com>
2022-11-03 19:08:44 +0100

    Add icon animation

--- src/app/page/landing-page/landing-page.component.ts
+++ src/app/page/landing-page/landing-page.component.ts
@@ -14,8 +14,8 @@ export class LandingPageComponent implements OnInit {
 
   showModal$:Observable<boolean> = new Observable<boolean>();
   wordList$:Observable<ReadonlyArray<Word>> = new Observable<ReadonlyArray<Word>>();
-
   wordsArrayList$:BehaviorSubject<ReadonlyArray<Word>> = new BehaviorSubject<ReadonlyArray<Word>>([]);
+  exportButtonDisabled:boolean = false;
 
   constructor(
     private readonly store:Store
@@ -24,7 +24,10 @@ export class LandingPageComponent implements OnInit {
   ngOnInit(): void {
     this.showModal$ = this.store.select(selectShowModalWord);
     this.wordList$= this.store.select(selectWords);
-    this.wordList$.subscribe(words => this.wordsArrayList$.next(words));
+    this.wordList$.subscribe(words => {
+      this.wordsArrayList$.next(words);
+      this.exportButtonDisabled = words.length === 0;
+    });
   }
 
   export(){
